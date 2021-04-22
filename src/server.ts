01c5d1372eda920e6167c3ddcea883dd74246ec8
Brenda Pereira <brendacamara@outlook.com>
2021-04-22 11:20:24 -0300

    feat: first route

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  return response.json({ message: "Olá NLW 5" });
+});
+
 app.listen(3333, () => console.log("Server is runnig on port 3333"));
