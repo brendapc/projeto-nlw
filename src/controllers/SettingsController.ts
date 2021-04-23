9fe9437ec6fc179761ab2cb2c00c8ddae6455063
Brenda Pereira <brendacamara@outlook.com>
2021-04-22 21:40:01 -0300

    refactor: remove imports

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepository";
 import { SettingsService } from "./../services/SettingsService";
 
 export class SettingsController {
   async create(request: Request, response: Response) {
     const { chat, username } = request.body;
 
-    const settingsService = new SettingsService( );
+    const settingsService = new SettingsService();
 
     const settings = await settingsService.create({ chat, username });
 
