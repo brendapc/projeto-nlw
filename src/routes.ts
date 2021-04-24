9e877a0f84d4092dac84a689195dbe38de1a93f8
Brenda Pereira <brendacamara@outlook.com>
2021-04-24 13:58:49 -0300

    feat: add GET messages route to search for all past messages

--- src/routes.ts
+++ src/routes.ts
@@ -12,3 +12,4 @@ const messagesController = new MessagesController();
 routes.post("/settings", settingsController.create);
 routes.post("/users", usersController.create);
 routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.loadByUser);
