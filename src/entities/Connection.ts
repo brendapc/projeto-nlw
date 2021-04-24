f2dd2ca80e43a75301f4f39646b423f819bba1fa
Brenda Pereira <brendacamara@outlook.com>
2021-04-24 20:45:13 -0300

    feat: create Connection Entity

---
+++ src/entities/Connection.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+import { User } from "./User";
+
+export class Connection {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  adminId: string;
+
+  @Column()
+  socketId: string;
+
+  @JoinColumn({ name: "userId" })
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  userId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
