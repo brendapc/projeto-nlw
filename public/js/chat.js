6e1e0bca82355fe86ed6b36ab4903fb074b37940
Brenda Pereira <brendacamara@outlook.com>
2021-04-24 20:28:55 -0300

    feat: emit first event

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,25 @@
-document.querySelector("#start_chat").addEventListener("click", (event) => {});
+document.querySelector("#start_chat").addEventListener("click", (event) => {
+  const chatHelp = document.getElementById("chat_help");
+  chatHelp.style.display = "none";
+
+  const chatInSupport = document.getElementById("chat_in_support");
+  chatInSupport.style.display = "block";
+  const socket = io();
+
+  const email = document.getElementById("email").value;
+  const text = document.getElementById("txt_help").value;
+
+  socket.on("connect", () => {
+    const params = {
+      email,
+      text,
+    };
+    socket.emit("clientFirstAccess", params, (call, err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(call);
+      }
+    });
+  });
+});
