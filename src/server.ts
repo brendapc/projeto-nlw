3a22fa6962612a9f58ebbe510a7a101c6844d4c5
Brenda Pereira <brendacamara@outlook.com>
2021-04-24 20:09:35 -0300

    feat: setting up socket configs

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,27 @@
-import express from "express";
+import express, { request, response } from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 import "./database";
 import { routes } from "./routes";
+import path from "path";
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
+
+const http = createServer(app); // criando protocolo http
+const io = new Server(http); // criando protocolo websocket
+
+io.on("connection", (socket: Socket) => {
+  console.log("Socket se conectou", socket.id);
+});
 
 app.use(express.json());
 
@@ -12,4 +31,4 @@ app.get("/", (request, response) => {
   return response.json({ message: "Olá NLW 5" });
 });
 
-app.listen(3333, () => console.log("Server is runnig on port 3333"));
+http.listen(3333, () => console.log("Server is runnig on port 3333"));
