58a7389b4ef59c6fa7209ca78220d63474fb596d
Brenda Pereira <brendacamara@outlook.com>
2021-04-24 21:02:58 -0300

    feat: create ConnectionService

---
+++ src/services/ConnectionsService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { Connection } from "./../entities/Connection";
+import { ConnectionsRepository } from "./../repositories/ConnectionsRepository";
+
+interface IConnectionCreate {
+  socketId: string;
+  userId: string;
+  adminId?: string;
+  id?: string;
+}
+
+export class ConnectionsService {
+  private _connectionsRepository: Repository<Connection>;
+  constructor() {
+    this._connectionsRepository = getCustomRepository(ConnectionsRepository);
+  }
+  async createImageBitmap({
+    socketId,
+    userId,
+    adminId,
+    id,
+  }: IConnectionCreate) {
+    const connection = this._connectionsRepository.create({
+      socketId,
+      userId,
+      adminId,
+      id,
+    });
+
+    await this._connectionsRepository.save(connection);
+
+    return connection;
+  }
+}
